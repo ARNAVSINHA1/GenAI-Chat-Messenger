365070c3a7e199909d1bfd1dc0ed5d8d00d718ae
ARNAVSINHA1 <arnavsinha.er@gmail.com>
2024-01-14 14:25:52 +0530

    arnava changing

--- chat-app-frontend/src/App.js
+++ chat-app-frontend/src/App.js
@@ -1,22 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { ChatContextProvider } from './context/ChatContext';
+import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
+import { ChatContextProvider } from './components/Chat/Chat';
 import Home from '/home/arnava/work/GenAI-Chat-Messenger/chat-app-frontend/src/components/Home/Home.js';
-// eslint-disable-next-line no-unused-vars
-import Chat from '@components/Chat/Chat';
-import { useChatContext } from './context/ChatContext';
+import { Chat as ChatComponent } from './components/Chat/Chat';
+import { useChatContext } from './components/Chat/Chat';
 
 function App() {
   const { isConnected } = useChatContext();
   return (
     <ChatContextProvider>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {isConnected && <Route path="/chat" component={Chat} />}
-          {!isConnected && <Redirect to="/" />}
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {isConnected && <Route path="/chat" element={<ChatComponent />} />}
+          {!isConnected && <Navigate to="/" replace />}
+        </Routes>
       </Router>
     </ChatContextProvider>
   );
--- chat-app-frontend/src/components/Chat/Chat.js
+++ chat-app-frontend/src/components/Chat/Chat.js
@@ -1,29 +1,25 @@
-import React, { useContext } from 'react';
-  import { ChatContext } from '../../context/ChatContext';
-  
- const Chat = () => {
-   const { messages, sendMessage } = useContext(ChatContext);
-   const handleSubmit = (event) => {
-    event.preventDefault();
-    const message = event.target.message.value;
-    sendMessage(message);
-    event.target.message.value = '';
-  }
-   return (
-     <div>
-       <h1>Chat with ReX</h1>
-       <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="message" placeholder="Type your message here" />
-        <button type="submit">Send</button>
-      </form>
-     </div>
-   );
-  }
-  
- export default Chat;
-  
+import React, { createContext, useState } from 'react';
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = (props) => {
+  const [isConnected, setIsConnected] = useState(false);
+
+  return (
+    <ChatContext.Provider value={{ isConnected, setIsConnected }}>
+      {props.children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = () => React.useContext(ChatContext);
+
+export const Chat = () => {
+  const { isConnected } = useChatContext();
+
+  return (
+    <div>
+      {isConnected ? <p>Chat is connected</p> : <p>Chat is not connected</p>}
+    </div>
+  );
+};
